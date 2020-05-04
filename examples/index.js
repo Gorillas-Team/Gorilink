27366e491a1ccdd22a52b8ce4ce65dfe833fe862
Psykka <36780964+Psykka@users.noreply.github.com>
2020-05-04 15:15:36 -0300

    Update example

--- examples/index.js
+++ examples/index.js
@@ -1,50 +1,67 @@
+// Importing Discord.js Client
 const { Client } = require('discord.js')
-const { GorilinkManager } = require('../src')
-const config = {
-  token: 'your-token',
-  prefix: '!',
-  nodes: [
-    {
-      host: 'localhost',
-      port: 2333,
-      password: 'youshallnotpass'
-    }
-  ]
-}
+// Importing GorilinkManager
+const { GorilinkManager } = require('gorilink')
 
+// Your lavalink node config
+const nodes = [
+  {
+    tag: 'Node 1', // optional
+    host: 'localhost',
+    port: 2333,
+    password: 'youshallnotpass'
+  }
+]
+
+// Instantiating discord.js client
 const client = new Client()
 
 client.on('ready', async () => {
+  // Creating GorilinkManager
   client.music = new GorilinkManager(client, config.nodes)
+    // Listens events
     .on('nodeConnect', node => {
       console.log(`${node.tag || node.host} - Lavalink connected with success.`)
     })
     .on('trackStart', event => {
-      event.player.textChannel.send(`Now playing \`${event.track.info.title}\``)
+      event.player.textChannel.send(`Now playing \`${res.track.info.title}\``)
     })
 
   console.log('Online on the client', client.user.username)
 })
 
+
 client.on('message', async (message) => {
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+  const prefix = '!'
+  const args = message.content.slice(prefix.length).trim().split(/ +/g)
   const cmd = args.shift().toLowerCase()
 
   if (cmd === 'play') {
+    // Tries to get the voice channel
+    const memberChannel = message.member.voice.channel.id
+    
+    // Checks if the member is on a voice channel
+    if(!memberChannel) return message.channel.send('You are not on a voice channel');
+
+    // Spawning lavalink player
     const player = await client.music.join({
       guild: message.guild.id,
-      voiceChannel: message.member.voice.channel.id,
+      voiceChannel: memberChannel,
       textChannel: message.channel
     })
 
+    // Getting tracks
     const { tracks } = await client.music.fetchTracks(args.join(' '))
 
+    // Adding in queue
     player.queue.add(tracks[0])
 
     message.channel.send('Added in queue: ' + tracks[0].info.title)
-
+    
+    // Playing
     if (!player.playing) return player.play()
   }
 })
 
-client.login(config.token)
+// Logging the bot
+client.login('your-token')

d012744af65dfaeef4b406096a5f63433001660f
vinicius augusto alves <viniciusfabrisilk@gmail.com>
2020-06-15 22:23:40 -0300

    Queue fix

--- src/lib/structures/Queue.js
+++ src/lib/structures/Queue.js
@@ -1,23 +1,15 @@
 module.exports = class Queue extends Array {
-  constructor () {
-    super()
-  }
+  constructor () { super() }
 
-  get duration () {
-    return this.reduce((acc, val) => acc + val.info.length, 0)
-  }
+  get duration () { return this.reduce((acc, val) => acc + val.info.length, 0) }
 
-  get empty () {
-    return this.length < 1
-  }
+  get empty () { return this.length < 1 }
 
-  first () {
-    return this[0]
-  }
+  first () { return this[0] }
 
   add (prop) { return this.push({ ...prop, index: this.length + 1 }) }
 
   removeFirst () { return this.shift() }
 
-  remove (index) { return this.filter(track => track.index != index) }
+  remove (index) { return this.splice(index, 1)[0] }
 }
